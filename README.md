0734605b6bb44e645a67cdbd654b2870b32c1516
feifa168 <2900093533@qq.com>
2019-01-03 15:53:32 +0800

    java调用路径问题，jar包中与本地文件调用方法不同

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # javapath
-java调用路径问题，jar包中与本地文件不同
+java调用路径问题，jar包中与本地文件调用方法不同
+
+## getClass().getResource(name)
+> 得到当前类的路径，读取文件需要相对于该class所在路径的相对位置
+>> IDEA调试和jar包中查找路径不同，IDEA调试下相对路径可以使用，但jar包中只能使用根路径/开头
+* name=""，得到的是当前类的路径
+* name="/"，得到的是根路径
+* name="./"，得到的是当前类的路径
+* name="../"，得到的是当前类的路径的上一层路径
+* name="file.txt"，得到的是该文件的路径
+* filename
+* ./filename
+* /filename
+
+## getClass().getResourceAsStream(name)
+同 getClass().getResource(name)
+
+## getClass().getClassLoader().getResource(name)
+> 得到的是根路径，IDEA中可以使用"filename"和"./filename"，但jar中只能使用"filename"，不能使用/符号
+* name=""，得到的是根路径
+* name="/"，错误，null
+* name="./"，错误，null
+* name="../"，错误，null
+* name="file.txt"，得到的是根路径下的文件
+* filename，IDEA和jar包中访问正常
+* ./filename，尽在IDEA中可以使用
+* /filename，都不能访问
+
+## getClass().getClassLoader().getResourceAsStream(name)
+同 getClass().getResource(name)
+
+## 本地文件，文件在包外面，可以使用相对路径
+> 前两种可以用，第三种不可用
+* filename，与jar在同级目录
+* ./filename，与jar在同级目录
+* ../filename，相对于jar包的上一级目录
+* /filename，错误，找不到文件
+
+## System.getProperty("user.dir")组装的本地文件，
+> 三种都可以用，System.getProperty("user.dir")得到当前jar包所在路径
+* filename，与jar在同级目录
+* ./filename，与jar在同级目录
+* ../filename，相对于jar包的上一级目录
+* /filename，与jar在同级目录
---
+++ src/main/java/com/path/App.java
@@ -0,0 +1,164 @@
+package com.path;
+
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+        App ap = new App();
+        ap.testUrl();
+    }
+
+    @Test
+    public void testPath() {
+        System.out.println(getClass().toString());
+
+        String name = "config.txt";
+
+        System.out.println(getClass().getResource(name));
+        System.out.println(getClass().getResource("./"+name));
+        System.out.println(getClass().getResource("../../"+name));
+        System.out.println(getClass().getResource("/"+name));
+
+        System.out.println(getClass().getClassLoader().getResource(name));
+        System.out.println(getClass().getClassLoader().getResource("./"+name));
+        System.out.println(getClass().getClassLoader().getResource("../"+name));
+        System.out.println(getClass().getClassLoader().getResource("/"+name));
+
+        System.out.println(getClass().getClassLoader().getResource("../../"+name));
+        System.out.println(getClass().getClassLoader().getResource("../../../"+name));
+        System.out.println(getClass().getClassLoader().getResource("../../../../"+name));
+
+        System.out.println(getClass().getResource("../../"+name));
+        System.out.println(getClass().getResource("../../../"+name));
+        System.out.println(getClass().getResource("../../../../"+name));
+    }
+
+    @Test
+    public void testMain() {
+        testUrl();
+    }
+
+    public void testUrl() {
+//        testCustomUrl("config.txt", true);
+//        testCustomUrl("config.txt", false);
+
+        testCustomUrl("config/config.txt", true, true);
+        testCustomUrl("config/config.txt", true, false);
+        testCustomUrl("config/config.txt", false, false);
+        //testCustomUrl("config/resourceconfig.txt");
+    }
+
+    private void testCustomUrl(String path, boolean isurl, boolean isClassLoader) {
+        System.out.println("==========================================");
+        System.out.println("class.getResource()                 = "+getClass().getResource(""));
+        System.out.println("class.getResource(/)                = "+getClass().getResource("/"));
+        System.out.println("class.getResource(./)                = "+getClass().getResource("./"));
+        System.out.println("class.getResource(../)                = "+getClass().getResource("../"));
+        System.out.println("class.getClassloader.getResource()  = "+getClass().getClassLoader().getResource(""));
+        System.out.println("class.getClassloader.getResource(/) = "+getClass().getClassLoader().getResource("/"));
+        System.out.println("==========================================\n");
+        displayUrlResource2(path, isurl, isClassLoader);
+        displayUrlResource2("./"+path, isurl, isClassLoader);
+        displayUrlResource2("../"+path, isurl, isClassLoader);
+        displayUrlResource2("../../"+path, isurl, isClassLoader);
+        displayUrlResource2("../../../"+path, isurl, isClassLoader);
+        displayUrlResource2("/"+path, isurl, isClassLoader);
+    }
+
+    public void displayUrlResource2(String path, boolean url, boolean isClassLoader) {
+        System.out.println("======="+path+", isurl="+(url?"true":"false") + ", isclassloader="+(isClassLoader?"true":"false"));
+
+        try {
+            InputStream ism = null;
+            if (url) {
+                URL ul = null;
+                if (isClassLoader) {
+                    ul = getClass().getClassLoader().getResource(path);
+                } else {
+                    ul = getClass().getResource(path);
+                }
+                if (ul == null) {
+                    System.out.println("url is null\n");
+                    return;
+                }
+                System.out.println("URL is "+ul.getFile());
+
+                ism = ul.openStream();
+            } else {
+                ism = new FileInputStream(path);
+            }
+
+            byte[] bts = new byte[128];
+            int readlen = ism.read(bts);
+            System.out.println(new String(bts, 0, readlen)+"\n");
+
+        } catch (FileNotFoundException e) {
+            //e.printStackTrace();
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void displayUrlResource(String path, boolean url) {
+        System.out.println("======="+path+", "+(url?"true":"false"));
+        try {
+            URL ul = getClass().getClassLoader().getResource(path);
+            if (ul == null) {
+                System.out.println("url is null");
+                return;
+            }
+            System.out.println("URL is "+ul.getFile());
+            try {
+                URI ui = ul.toURI();
+                if (ui != null) {
+                    System.out.println("URI is "+ui.toString());
+                }
+                Path pt = null;
+                if (url) {
+                    pt = Paths.get(ui);
+                } else {
+                    pt = Paths.get(path);
+                }
+                FileChannel fc = FileChannel.open(pt);
+                ByteBuffer bf = ByteBuffer.allocate(256);
+                boolean hasData = true;
+                while (hasData) {
+                    bf.clear();
+                    while (bf.hasRemaining()) {
+                        if (fc.read(bf) < 1) {
+                            hasData = false;
+                            break;
+                        }
+                    }
+                    bf.flip();
+                    System.out.println(new String(bf.array()));
+                }
+            } catch(FileSystemNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch(NullPointerException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
